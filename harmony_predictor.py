3aa86634d37c71c1f0cd2030c26e2b22526efc10
anacrusis24 <ajv131@gmail.com>
2021-03-04 16:23:07 -0600

    plays tonic for each measure in each section

--- harmony_predictor.py
+++ harmony_predictor.py
@@ -4,87 +4,81 @@ import cred
 import Harmony as harm
 import time
 import musicalbeeps
+import numpy as np
 
 scope = "user-read-playback-state,user-modify-playback-state"
 sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
 
-# Shows playing devices
-audio_features = sp.audio_features('spotify:track:7a9aeLVkn7DIqFjbanKz0k')
-
-key = audio_features[0]['key']
-mode = audio_features[0]['mode']
-tempo = audio_features[0]['tempo']
-time_signature = audio_features[0]['time_signature']
-time_per_measure = time_signature * 1/tempo * 60
-
-#key mapping to letters
-if key == 0:
-    key = 'C'
-elif key == 1:
-    key = 'C#'
-elif key == 2:
-    key = 'D'
-elif key == 3:
-    key = 'Eb'
-elif key == 4:
-    key = 'E'
-elif key == 5:
-    key = 'F'
-elif key == 6:
-    key = 'F#'
-elif key == 7:
-    key = 'G'
-elif key == 8:
-    key = 'G#'
-elif key == 9:
-    key = 'A'
-elif key == 10:
-    key = 'Bb'
-elif key == 11:
-    key = 'B'
-
-if mode == 0:
-    mode = 'Minor'
-elif mode == 1:
-    mode = "Major"
-
-harmony = harm.Harmony(key, mode)
-
-tonic_chord = harmony.triad_harmony(key)
-IV_chord = harmony.triad_harmony(harmony.scale[3])
-major_dom_chord = harmony.major_dom(harmony.scale[4])
-VI_chord = harmony.triad_harmony(harmony.scale[5])
-
-tswift_progression = [tonic_chord, major_dom_chord, VI_chord, IV_chord]
-i = 0
-
 player = musicalbeeps.Player(volume = 0.3,
                             mute_output = False)
 
-# while(True):
-#     print(tswift_progression[i])
-#     player.play_note(tswift_progression[i][0], time_per_measure)
-#     i += 1
-#     if i == 4:
-#         i = 0
-
-
-print(audio_features)
-
-
-
-# To play an A on default octave n°4 for 0.2 seconds
-player.play_note("A", 5)
-
-# # To play a G flat on octave n°3 for 2.5 seconds
-player.play_note("G3b", 2.5)
-
-# # To play a F sharp on octave n°5 for the default duration of 0.5 seconds
-player.play_note("F5#")
-
 analysis = sp.audio_analysis('spotify:track:7a9aeLVkn7DIqFjbanKz0k')
-# print(analysis['sections'])
+section_start_times = []
+section_durations = []
+section_keys = []
+section_modes = []
+section_tempos = []
+section_time_signature = []
 
 for section in analysis['sections']:
-    print(section['start'])
-    print(section['key'])
+    section_start_times.append(section['start'])
+    section_durations.append(section['duration'])
+    section_keys.append(section['key'])
+    section_modes.append(section['mode'])
+    section_tempos.append(section['tempo'])
+    section_time_signature.append(section['time_signature'])
+
+section_start_times = np.array(section_start_times)
+section_durations = np.array(section_durations)
+section_tempos = np.array(section_tempos)
+section_time_signature = np.array(section_time_signature)
+section_num_measures = section_durations * (section_tempos/60) / section_time_signature
+# section_num_measures = np.round(section_num_measures)
+
+#key mapping to letters
+for i in range(len(section_keys)):
+    if section_keys[i] == 0:
+        section_keys[i] = 'C'
+    elif section_keys[i] == 1:
+        section_keys[i] = 'C#'
+    elif section_keys[i] == 2:
+        section_keys[i] = 'D'
+    elif section_keys[i] == 3:
+        section_keys[i] = 'Eb'
+    elif section_keys[i] == 4:
+        section_keys[i] = 'E'
+    elif section_keys[i] == 5:
+        section_keys[i] = 'F'
+    elif section_keys[i] == 6:
+        section_keys[i] = 'F#'
+    elif section_keys[i] == 7:
+        section_keys[i] = 'G'
+    elif section_keys[i] == 8:
+        section_keys[i] = 'G#'
+    elif section_keys[i] == 9:
+        section_keys[i] = 'A'
+    elif section_keys[i] == 10:
+        section_keys[i] = 'Bb'
+    elif section_keys[i] == 11:
+        section_keys[i] = 'B'
+
+for i in range(len(section_modes)):
+    if section_modes[i] == 0:
+        section_modes[i] = 'Minor'
+    elif section_modes[i] == 1:
+        section_modes[i] = "Major"
+
+for i in range(len(section_durations)):
+    harmony = harm.Harmony(section_keys[i], section_modes[i])
+    tonic_chord = harmony.triad_harmony(section_keys[i])
+    IV_chord = harmony.triad_harmony(harmony.scale[3])
+    major_dom_chord = harmony.triad_harmony(harmony.scale[4])
+    VI_chord = harmony.triad_harmony(harmony.scale[5])
+    tswift_progression = [tonic_chord, major_dom_chord, VI_chord, IV_chord]
+
+    for j in range(int(section_num_measures[i])):
+        k = j
+        if k > len(tswift_progression) - 1:
+            k = j % len(tswift_progression)
+        player.play_note(tswift_progression[k][0], section_durations[i]/section_num_measures[i])
+        print(tswift_progression[k])
